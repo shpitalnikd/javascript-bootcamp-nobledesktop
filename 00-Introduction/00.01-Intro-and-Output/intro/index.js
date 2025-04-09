e5ccdc60e1993269e4e0be4976aa6f102e4e498d
steven <shotta3xd@gmail.com>
2025-04-08 20:22:53 -0400

    added intro demo

---
+++ 00-Introduction/00.01-Intro-and-Output/intro/index.js
@@ -0,0 +1,29 @@
+// alert(" HELLO I AM YOUR FIRST TASTE OF JS !");
+
+// console.log("Hello I AM IN YOUR CONSOLE ?");
+
+const x = 10;
+
+console.log(x);
+
+// Leave comments like hey Steven just taught use how to make comments.
+
+/**
+ *  Note 1
+ *  Note 2
+ *  Note 3
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
