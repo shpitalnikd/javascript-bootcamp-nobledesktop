421f2aab805460c52b1941cc6363dc0741bb7afb
steven <shotta3xd@gmail.com>
2025-04-10 19:43:09 -0400

    Demo for string number and boolean

---
+++ 01-VARIABLES/01.01-Strings-Numbers-Booleans/demo.js
@@ -0,0 +1,61 @@
+console.log("Hello World !");
+/**
+ Const - immutability never changing
+ Let - mutable change it
+ Var - Break(s) the rules
+ */
+const value = `🥲& %$*)*%3095`;
+("123 45 678");
+let greeting = "Hello my name is Steven";
+// 3 different equal signs
+/*
+  =  <-- assignment operator
+  == <-- test for truthy and falsy values 
+  === <-- the commparsion operator
+  !== <-- are these things not equal 
+*/
+// Strings
+
+greeting = " :) Hello guys I wanna change";
+console.log(greeting);
+// numbers
+const num = 2;
+const str = "2";
+console.log(typeof greeting, typeof true);
+
+const addition = 12 / 2;
+const subtraction = 12 - 2;
+const multiplcation = 12 * 2;
+const divide = 12 / 2;
+
+console.log("divide", addition !== subtraction);
+console.log("addition", subtraction, value);
+console.log("multiplcation", multiplcation, value);
+console.log("subtraction", divide, value);
+console.log("Power", 12 ** 2, value);
+
+console.log("PEMDAS", (1 + 2) * (24 / 1) - (12 + 8));
+// boolean
+
+console.log(true && false);
+console.log(!(true && true));
+console.log(!(true || true));
+console.log(!(true || false));
+// null // undefined // boolean
+
+/*
+
+True and False
+
+or  - a condition: ||
+
+and - a conditon: &&
+
+inverse - bang operator: !
+
+*/
+
+// undefined
+console.log(undefined);
+console.log(null);
+// null
