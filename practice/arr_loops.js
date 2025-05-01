6df5873a6a01888a64b2bc440438d43044bc21de
steven <shotta3xd@gmail.com>
2025-05-01 18:19:41 -0400

    added new files

---
+++ practice/arr_loops.js
@@ -0,0 +1,24 @@
+// 12345
+// 01234
+const arr = [1, 2, 3];
+let myarr = ["a", "b", "c"];
+
+console.log("arr", arr, "myarr", myarr);
+
+console.log("arr", typeof arr, "myarr", typeof myarr, typeof console);
+
+//    key  = value
+const key = 3;
+
+const obj = {
+  // key : value pair system
+  steven: 31,
+  carmen: 19,
+  david: 18,
+};
+console.log(obj.david);
+
+// initialization ; Condition ; increment
+for (let i = 0; i <= myarr.length - 1; i++) {
+  console.log(myarr[i]);
+}
